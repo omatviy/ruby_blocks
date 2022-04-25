ad27f5bfcab795ae592f6c0815e756faa2ad8bd2
Sasha Matviy <omatviy@gmail.com>
2022-04-25 14:15:49 +0300

    Lesson 7

--- course.rb
+++ course.rb
@@ -17,5 +17,30 @@ end
 
 roll do |name, number|
   puts "Your name is #{name}. You rolled a #{number}"
-  number*2
+  number * 2
 end
+
+class Integer
+  def n_times
+    i = 0
+    while i < self
+      yield i
+      i += 1
+    end
+  end
+end
+
+10.n_times { |i| puts "Echo #{i}" }
+
+class Array
+  def each
+    i = 0
+    while i < self.size
+      yield self[i]
+      i += 1
+    end
+  end
+end
+
+week = %w[monday tuesday wensday thursday friday saturday sunday]
+week.each {|d| puts d.capitalize}
